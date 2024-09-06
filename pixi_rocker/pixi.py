82199ee643c8a3d3f1a8326595789d7b297604c7
Austin Gregg-Smith <blooop@gmail.com>
2024-09-06 19:01:36 +0100

    fix: linting errors and add tests

--- pixi_rocker/pixi.py
+++ pixi_rocker/pixi.py
@@ -1,18 +1,4 @@
-import grp
-import os
-import docker
-import getpass
-import pwd
 import pkgutil
-from pathlib import Path
-from shlex import quote
-import subprocess
-import sys
-
-from pathlib import Path
-from collections import defaultdict
-import yaml
-import toml
 from rocker.extensions import RockerExtension
 
 
@@ -25,23 +11,22 @@ class PixiExtension(RockerExtension):
         self._env_subs = None
         self.name = PixiExtension.get_name()
 
-    def get_preamble(self, cliargs):
-        return ""
-
     def get_snippet(self, cliargs):
         snippet = pkgutil.get_data("pixi_rocker", "templates/curl_snippet.Dockerfile").decode(
             "utf-8"
         )
         return snippet
-    
+
     def get_user_snippet(self, cliargs):
-        snippet = pkgutil.get_data("pixi_rocker", f"templates/{self.name}_snippet.Dockerfile").decode(
-            "utf-8"
-        )
+        snippet = pkgutil.get_data(
+            "pixi_rocker", f"templates/{self.name}_snippet.Dockerfile"
+        ).decode("utf-8")
         return snippet
 
     @staticmethod
-    def register_arguments(parser, defaults={}):
+    def register_arguments(parser, defaults=None):
+        if defaults is None:
+            defaults = {}
         parser.add_argument(
             f"--{PixiExtension.get_name()}",
             action="store_true",
